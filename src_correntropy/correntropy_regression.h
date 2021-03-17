1ef0fe9b4b969868814d7927c05f88d8bde4bda5
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-03-17 15:57:49 +0100

    First "version".
      Mathieu

---
+++ src_correntropy/correntropy_regression.h
@@ -0,0 +1,31 @@
+#ifndef INCLUDE_CORRENTROPY_REGRESSION_H
+#define INCLUDE_CORRENTROPY_REGRESSION_H
+
+
+
+/*
+  We have a number of function f_j for 1 <= j <= m
+  A number of values x_i for 1 <= i <= n and a fitting value b_i
+  ---
+  So, we want to find the coefficients beta_j such that
+  b_i is near sum_j beta_j f_j(x_i)
+  ---
+  There are many ways to do this fitting, here we consider the correntropy.
+  That is we want to maximize
+  sum_i g(b_i, sum_j beta_j f_j(x_i))
+  with g(x, y) = exp(-(x-y)^2 / 2 \sigma^2)
+  ---
+  The classical algorithm is the half-quadratic optimization.
+ */
+template<typename T>
+std::vector<T> Computre_Regression(MyVector<T> const& ListValue, MyMatrix<T> const& ListFct, double const& sigma)
+{
+
+
+}
+
+
+
+
+
+#endif
